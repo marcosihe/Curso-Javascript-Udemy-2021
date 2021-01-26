4cd9b39a017491445ef8269f06c90a8bc44afe6b
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-01-26 11:05:31 -0300

    Se trabajó sobre callbacks y en una parte de promesas

--- Curso-de-Javascript-Udemy-master/11. asincronismo/02. callbacks/tutorial/main.js
+++ Curso-de-Javascript-Udemy-master/11. asincronismo/02. callbacks/tutorial/main.js
@@ -19,3 +19,31 @@ function calcularSuma(valores, callback){
 console.log(calcularSuma([1,2,3,4,5], function(){
     console.log('Aquí termina la función');
 }));
+
+function calcularSuma2(valores, callback){
+    const res = valores.reduce((acc, value) => acc += value, 0);
+    callback(res);
+    console.log('ya se mandó a llamar el callback');
+}
+
+calcularSuma2([2,4,6,8], res => {
+    console.log('el resultado es: ', res);
+});
+
+function calcularSuma3(valores, callback, errorCallback){
+    const res = valores.reduce((acc, value) => {
+        if(isNaN(value)){
+            errorCallback(`El valor ${value} no es numérico`);
+            return false;
+        }else{
+            return acc+= value;
+        }
+    });
+    callback(res);
+}
+
+calcularSuma3([1,1,1,5], res => {
+    if(res) console.log('resultado de calcularSuma3 =', res);
+}, error => {
+    console.error(error);
+});
